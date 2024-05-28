b5c3e4abc16b5ade14edffbd1a8b95b98282af0f
Olesa Semenova <semenovmaolesa@gmail.com>
2024-05-28 14:42:59 +0300

    Имплементация логики игры

--- script.js
+++ script.js
@@ -19,11 +19,36 @@
 //   console.log(document.querySelector('.number-input').value);
 // };
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.question').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(guessingNumber, typeof guessingNumber);
 
   if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Введите число!';
+  } else if (guessingNumber === secretNumber) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+  } else if (guessingNumber > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком много!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guessingNumber < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком мало!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Game Over!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
