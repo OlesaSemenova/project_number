30537691b3367fd9c618b79782c7ffab198a90c7
Olesa Semenova <semenovmaolesa@gmail.com>
2024-05-29 15:37:41 +0300

    Добавление функционала лучший результат без ошибки_2

--- script.js
+++ script.js
@@ -23,8 +23,6 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
-document.querySelector('.question').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
   console.log(guessingNumber, typeof guessingNumber);
@@ -38,6 +36,7 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.guess-message').textContent = 'Правильно!';
     document.querySelector('body').style.backgroundColor = 'rgb(9, 250, 21)';
     document.querySelector('.question').style.width = '50rem';
+    document.querySelector('.question').textContent = secretNumber;
 
     if (score > highscore) {
       highscore = score;
@@ -47,6 +46,7 @@ document.querySelector('.check').addEventListener('click', function () {
     //Too high
   } else if (guessingNumber > secretNumber) {
     if (score > 1) {
+      score--;
       document.querySelector('.guess-message').textContent = 'Слишком много!';
       document.querySelector('.score').textContent = score;
     } else {
@@ -57,8 +57,8 @@ document.querySelector('.check').addEventListener('click', function () {
     //To low
   } else if (guessingNumber < secretNumber) {
     if (score > 1) {
-      document.querySelector('.guess-message').textContent = 'Слишком мало!';
       score--;
+      document.querySelector('.guess-message').textContent = 'Слишком мало!';
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.guess-message').textContent = 'Game Over!';
